094960960397876dd6e55fbb920f374629c22bc6
pandenok <dimitri.shatunin@gmail.com>
2020-11-15 22:37:08 +0100

    Fix bug of Unknown Flickr user and include Flickr API errors

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,11 +2,16 @@ class StaticPagesController < ApplicationController
   require 'flickr'
 
   def index
-    flickr = Flickr.new
-    unless params[:user_id].blank?
-      @photos = flickr.photos.search(user_id: params[:user_id])
-    else
-      @photos = flickr.photos.getRecent
+    begin
+      flickr = Flickr.new
+      unless params[:user_id].blank?
+        @photos = flickr.photos.search(user_id: params[:user_id])
+      else
+        @photos = flickr.photos.getRecent
+      end
+    rescue StandardError => e
+      flash[:alert] = "#{e.class}: #{e.message}. Please try again..."
+      redirect_to root_path
     end
   end
 end
